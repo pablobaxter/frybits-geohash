405de1b1c7f043a7dd99e8f1586ef114ed02bac3
Pablo Baxter <pablobaxter@msn.com>
2020-01-28 01:50:23 -0800

    Expecting passing tests

--- geohash/src/commonTest/kotlin/com.frybits.geohash/test/LatLonUtilsTest.kt
+++ geohash/src/commonTest/kotlin/com.frybits.geohash/test/LatLonUtilsTest.kt
@@ -8,7 +8,6 @@ import kotlin.random.Random
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertFailsWith
-import kotlin.test.fail
 
 /**
  * Frybits
@@ -95,10 +94,5 @@ class LatLonUtilsTest {
         val combinedBits3 = latLonBits(TEST_COMBINED_BITS_3)
 
         assertEquals(latLonBits3, combinedBits3)
-    }
-
-    @Test
-    fun failTest() {
-        fail("This is a failure")
     }
 }
